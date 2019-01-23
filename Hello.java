c97b8ab7f2282e694dd4445ae8241d0cadc12f95
ChristinaMattern <46796474+ChristinaMattern@users.noreply.github.com>
2019-01-23 18:47:45 -0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,10 @@
-
+import java.util.Scanner;
 public class Hello {
 	public static void main(String[] args){
-		System.out.print("Christina Mattern");
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("What is your name?");
+		String name = keyboard.nextLine();
+		System.out.println("Hello, " + name);
 		system.out.prinln("Hello, I am Nola");
 	}
 
